ef3c8b616daa4f630d2e1d4c547f335ec1fabd69
zhangxunice <2462832901@qq.com>
2019-07-08 10:15:21 +0800

    工具类

--- src/main/java/com/example/collagecall/controller/StudentController.java
+++ src/main/java/com/example/collagecall/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.collagecall.controller;
 
 import com.example.collagecall.dao.StudentDao;
 import com.example.collagecall.entity.Student;
+import com.example.collagecall.utils.CollegeJSONResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,13 @@ public class StudentController {
 
     @Autowired
     private StudentDao studentDao;
+
+//    @GetMapping("/wxlogin")
+//    public CollegeJSONResult wxlogin(String userid){
+//        System.out.println(userid);
+//        return CollegeJSONResult.ok();
+//    }
+
     @GetMapping("/test")
     public List<Student> test(){
         List<Student> studentList=studentDao.selectList(null);
